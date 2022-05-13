0a943e98f9db29059e2ee12ce25a382f4945528c
Thaysa <thay.fernnanda@gmail.com>
2022-05-13 20:21:24 -0300

    adiciona correcao do lint

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -3,7 +3,6 @@ import Title from './Title';
 import PlanetCard from './PlanetCard';
 import planets from '../data/planets';
 
-
 class SolarSystem extends React.Component {
   render() {
     return (
